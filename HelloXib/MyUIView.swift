d062b1f02488e73de68a6a8ea2a3a976cbe31f4c
Huang Grace <huang.yahui.grace@gmail.com>
2019-11-30 16:59:05 +0800

    Initial Commit

---
+++ HelloXib/MyUIView.swift
@@ -0,0 +1,21 @@
+//
+//  MyUIView.swift
+//  HelloXib
+//
+//  Created by grace on 2019/11/30.
+//  Copyright © 2019 grace. All rights reserved.
+//
+
+import UIKit
+
+class MyUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
